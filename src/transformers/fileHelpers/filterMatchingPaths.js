9e7c643361dc540ffcc9f27bf0e18fa88bfe5692
Clint Ayres <clinton.ayres@gmail.com>
2016-08-16 16:32:08 -0400

    Update filterMatchingPaths.js

--- src/transformers/fileHelpers/filterMatchingPaths.js
+++ src/transformers/fileHelpers/filterMatchingPaths.js
@@ -4,12 +4,7 @@ import removeExtension from './removeExtension';
 export default function filterMatchingPaths(basedir, filePath) {
   const normalizedFilePath = removeExtension(filePath);
   return (path) => {
-    const value = path.value.value;
-    if (typeof value !== 'string') {
-      return false;
-    }
-
-    const testPath = removeExtension(resolve(basedir, value));
+    const testPath = removeExtension(resolve(basedir, path.value.value));
     return testPath === normalizedFilePath;
   };
 }
