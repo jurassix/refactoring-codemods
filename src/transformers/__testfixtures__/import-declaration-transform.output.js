20833b76d316690de896b582767fd109515a4916
jspaine <painejs@hotmail.com>
2017-04-09 14:12:41 +0200

    fix parent paths, dot filenames

--- src/transformers/__testfixtures__/import-declaration-transform.output.js
+++ src/transformers/__testfixtures__/import-declaration-transform.output.js
@@ -1,9 +1,13 @@
 import bar from './new/path/to/bar';
 import foo from './new/path/to/foo';
+import baz from '../new/baz';
+import bazQux from './new/baz.qux';
 
 export * from './new/path/to/bar';
 export {default as bar} from './new/path/to/bar';
 export {default as foo} from './new/path/to/foo';
+export * from '../new/baz';
+export * from './new/baz.qux';
 export const num = 1;
 export const filePath = './bar';
 
--- src/transformers/fileHelpers/filterMatchingPaths.js
+++ src/transformers/fileHelpers/filterMatchingPaths.js
@@ -2,9 +2,9 @@ import {resolve} from 'path';
 import removeExtension from './removeExtension';
 
 export default function filterMatchingPaths(basedir, filePath) {
-  const normalizedFilePath = removeExtension(filePath);
+  const normalizedFilePath = removeExtension(resolve(filePath));
   return (path) => {
-    const testPath = removeExtension(resolve(basedir, path.value.value));
+    const testPath = resolve(basedir, path.value.value);
     return testPath === normalizedFilePath;
   };
 }
