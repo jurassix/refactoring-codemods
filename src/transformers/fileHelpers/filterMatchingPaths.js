e1355b9e3ab7aabb0192d40ce74d7459dc0cd431
James <painejs@hotmail.com>
2017-04-09 18:32:49 +0200

    use normalize instead of resolve

--- src/transformers/fileHelpers/filterMatchingPaths.js
+++ src/transformers/fileHelpers/filterMatchingPaths.js
@@ -1,8 +1,8 @@
-import {resolve} from 'path';
+import {normalize, resolve} from 'path';
 import removeExtension from './removeExtension';
 
 export default function filterMatchingPaths(basedir, filePath) {
-  const normalizedFilePath = removeExtension(resolve(filePath));
+  const normalizedFilePath = removeExtension(normalize(filePath));
   return (path) => {
     const testPath = resolve(basedir, path.value.value);
     return testPath === normalizedFilePath;
